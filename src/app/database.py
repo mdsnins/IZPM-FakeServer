4c856516ab5b38e379493a3397a8a00ef245a96b
mdsnins <payload@kaist.ac.kr>
2021-05-05 03:00:03 +0900

    Add database connector

---
+++ src/app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orgm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+import .config
+
+engine = create_engine(config.SQLITE_DB, convert_unicode = True)
+db_session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
