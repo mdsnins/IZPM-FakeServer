e86ff205de8bd40ed473ee639e5ac5ebd1725f36
mdsnins <payload@kaist.ac.kr>
2021-05-05 16:40:08 +0900

    Implemented marking features

--- src/app/model.py
+++ src/app/model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
-from .database import Base
+from .database import Base, db_session
 
 class User(Base):
     __tablename__ = "USER"
@@ -25,13 +25,61 @@ class User(Base):
     reads = relationship('Mail', secondary = "MAIL_READ")
     stars = relationship('Mail', secondary = "MAIL_STAR")
 
+    m_names = []
+    m_unreads = []
+
     def __init__(self):
         self.all_unread_count = 0
         self.star_count = 0
 
         self.member_names = "-|장원영|미야와키 사쿠라|조유리|최예나|안유진|야부키 나코|권은비|강혜원|혼다 히토미|김채원|김민주|이채연"
-        self.reads = "-1|0|0|0|0|0|0|0|0|0|0|0|0"  # First column is for padding
-        self.stars = "-1|0|0|0|0|0|0|0|0|0|0|0|0" # First column is for padding
+        self.member_unreads = "-1|0|0|0|0|0|0|0|0|0|0|0|0"  # First column is for padding
+        self.member_stars = "-1|0|0|0|0|0|0|0|0|0|0|0|0" # First column is for padding
+
+    def read_mail(self, mail_id):
+        mail = Mail.query.get(mail_id)
+        
+        if not mail:
+            return -1 # Error
+
+        if mail in self.reads:
+            return 0 # Already read
+
+        self.reads.append(mail)
+        self.m_unreads[mail.member_id] -= 1
+        self.member_unreads = '|'.join(self.m_unreads)
+        db_session.commit()
+        return 1 # Successfully processed
+
+    def star_mail(self, mail_id):
+        mail = Mail.query.get(mail_id)
+        
+        if not mail:
+            return -1 # Error
+
+        if mail in self.stars:
+            return 0 # Already starrerd
+
+        self.stars.append(mail)
+        self.m_stars[mail.member_id] += 1
+        self.member_stars = '|'.join(self.m_unreads)
+        db_session.commit()
+        return 1 # Successfully processed
+
+    def unstar_mail(self, mail_id):
+        mail = Mail.query.get(mail_id)
+        
+        if not mail:
+            return -1 # Error
+
+        if not mail in self.stars:
+            return 0 # not in stars
+
+        self.stars.remove(mail)
+        self.m_stars[mail.member_id] -= 1
+        self.member_stars = '|'.join(self.m_unreads)
+        db_session.commit()
+        return 1 # Successfully processed
 
 class Mail(Base):
     __tablename__ = "MAIL"
