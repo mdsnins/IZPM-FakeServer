89af4e0cc2379d6b48599a3dee37f516827b7e26
mdsnins <payload@kaist.ac.kr>
2021-05-05 17:37:29 +0900

    Integrate all_unread_count and all_star_count into member_unreads and member_stars

--- src/app/model.py
+++ src/app/model.py
@@ -7,16 +7,13 @@ class User(Base):
     __tablename__ = "USER"
     
     id = Column(Integer, primary_key = True)
-    user_id = Column(String(32), primary_key = True)
+    user_id = Column(String(32), unique  = True)
     nickname = Column(String(64), unique = False)
     gender = Column(String(8), unique = False)
     country_code = Column(String(2), unique = False)
     prefecture_id = Column(Integer, unique = False)
     birthday = Column(String(8), unique = False)
     member_id = Column(Integer, unique = False)
-
-    all_unread_count = Column(Integer, unique = False)
-    star_count = Column(Integer, unique = False)
 
     member_names = Column(String(180), unique = False)
     member_unreads = Column(String(84), unique = False)
@@ -33,11 +30,11 @@ class User(Base):
         self.star_count = 0
 
         self.member_names = "-|장원영|미야와키 사쿠라|조유리|최예나|안유진|야부키 나코|권은비|강혜원|혼다 히토미|김채원|김민주|이채연"
-        self.member_unreads = "-1|0|0|0|0|0|0|0|0|0|0|0|0"  # First column is for padding
-        self.member_stars = "-1|0|0|0|0|0|0|0|0|0|0|0|0" # First column is for padding
+        self.member_unreads = "0|0|0|0|0|0|0|0|0|0|0|0|0"  # First column is for total
+        self.member_stars = "0|0|0|0|0|0|0|0|0|0|0|0|0" # First column is for total
 
-    def read_mail(self, mail_id):
-        mail = Mail.query.get(mail_id)
+    def read_mail(self, id):
+        mail = Mail.query.get(id)
         
         if not mail:
             return -1 # Error
@@ -47,12 +44,13 @@ class User(Base):
 
         self.reads.append(mail)
         self.m_unreads[mail.member_id] -= 1
+        self.m_unreads[0] -= 1
         self.member_unreads = '|'.join(self.m_unreads)
         db_session.commit()
         return 1 # Successfully processed
 
-    def star_mail(self, mail_id):
-        mail = Mail.query.get(mail_id)
+    def star_mail(self, id):
+        mail = Mail.query.get(id)
         
         if not mail:
             return -1 # Error
@@ -62,12 +60,13 @@ class User(Base):
 
         self.stars.append(mail)
         self.m_stars[mail.member_id] += 1
+        self.m_stars[0] += 1 
         self.member_stars = '|'.join(self.m_unreads)
         db_session.commit()
         return 1 # Successfully processed
 
-    def unstar_mail(self, mail_id):
-        mail = Mail.query.get(mail_id)
+    def unstar_mail(self, id):
+        mail = Mail.query.get(id)
         
         if not mail:
             return -1 # Error
@@ -77,19 +76,20 @@ class User(Base):
 
         self.stars.remove(mail)
         self.m_stars[mail.member_id] -= 1
+        self.m_stars[0] -= 1
         self.member_stars = '|'.join(self.m_unreads)
         db_session.commit()
         return 1 # Successfully processed
 
-    def is_read(self, mail_id):
-        mail = Mail.query.get(mail_id)
+    def is_read(self, id):
+        mail = Mail.query.get(id)
         if not mail:
             return False
         
         return mail in self.reads
 
-    def is_star(self, mail_id):
-        mail = Mail.query.get(mail_id)
+    def is_star(self, id):
+        mail = Mail.query.get(id)
         if not mail:
             return False
         
@@ -99,7 +99,7 @@ class Mail(Base):
     __tablename__ = "MAIL"
 
     id = Column(Integer, primary_key = True)
-    mail_id = Column(String(8), primary_key = True)
+    mail_id = Column(String(8), unique = True)
     subject = Column(String(80), unique = False)
     content = Column(String(80), unique = False)
     
@@ -108,7 +108,6 @@ class Mail(Base):
     time = Column(DateTime, unique = False)
     datetime = Column(DateTime, unique = False)
     is_image = Column(Boolean, unique = False)
-
 
 # Association Tables
 class MailReads(Base):
