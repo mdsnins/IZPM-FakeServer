a8b3ccea381a099f6040f0c6883b14899fb028fd
mdsnins <payload@kaist.ac.kr>
2021-05-05 18:59:34 +0900

    Preparing Flask application

---
+++ src/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from os import urandom
+
+import .config
+import .database
+
+from .api import router as api_router
+
+app = Flask(__name__)
+app.config["SERVER_NAME"] = config.SERVER_NAME
+app.secret_key = urandom(16)
+
+app.register_blueprint(api_router, url_prefix='/v1')
+
