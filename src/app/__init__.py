0c43362fbf2269d80e12649eb8797987c54700bc
mdsnins <payload@kaist.ac.kr>
2021-05-05 19:44:52 +0900

    Add a logic for inserting test data into db

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,9 +1,12 @@
+import datetime
+
 from flask import Flask
 from os import urandom
 
 from . import config
 from . import database
-
+from . import model
+    
 from .api import router as api_router
 
 app = Flask(__name__)
@@ -14,5 +17,30 @@ app.register_blueprint(api_router, url_prefix='/v1')
 
 
 def init_db():
-    from . import model
     database.Base.metadata.create_all(bind = database.engine)
+
+def test_db():
+    u = model.User()
+    u.user_id = "AAAABBBB"
+    u.nickname = "테스트"
+    u.gender = "male"
+    u.country_code = "KR"
+    u.prefecture_id = -1
+    u.birthday = "20181029"
+    membeR_id = 1
+
+    database.db_session.add(u)
+
+    m = model.Mail()
+    m.mail_id = "m1"
+    m.subject = "테스트 메일"
+    m.content = "이건 테스트 메일입니다"
+    m.member_id = 1
+    m.time = datetime.datetime.now()
+    m.datetime = datetime.datetime.now()
+    m.is_image = True
+
+    database.db_session.add(m)
+
+    db_session.commit()
+    
--- src/run.py
+++ src/run.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
         app.run(host = "0.0.0.0", port = 10000, debug = True)
     elif argv[1] == "initdb":
         init_db()
+    elif argv[1] == "testdb":
+        test_db()
