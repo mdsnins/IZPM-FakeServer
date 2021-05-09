0634e70d7a54e9a738cef0ad904117c281c16b00
mdsnins <payload@kaist.ac.kr>
2021-05-09 20:17:56 +0900

    Add uWSGI configuration

--- src/run.py
+++ src/run.py
@@ -5,7 +5,7 @@ from app import *
 if __name__ == "__main__":
     if argv[1] == "run":
         init()
-        app.run(host = "0.0.0.0", port = 10000, debug = True)
+        app.run(host = "0.0.0.0", port = 1000, debug = True)
     elif argv[1] == "initdb":
         init_db()
     elif argv[1] == "testdb":
