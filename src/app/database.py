fbd74a8514ab4d3d85d55ce07c1f5688efd8069c
mdsnins <payload@kaist.ac.kr>
2021-05-26 08:54:55 +0900

    Migrate from sqlite3 to mysql

--- src/app/database.py
+++ src/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from . import config
 
-engine = create_engine(config.SQLITE_DB, convert_unicode = True)
+engine = create_engine(config.MYSQL_DB, convert_unicode = True)
 db_session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
 
 Base = declarative_base()
